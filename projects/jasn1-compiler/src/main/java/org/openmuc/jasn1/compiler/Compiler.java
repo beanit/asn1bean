ed1a5aeabb0c0be2b177ad2ae1714feced4c241a
Stefan Feuerhahn <stefan.feuerhahn@ise.fraunhofer.de>
2017-08-28 00:11:10 +0200

    prepare new release 1.8.3-SNAPSHOT

--- projects/jasn1-compiler/src/main/java/org/openmuc/jasn1/compiler/Compiler.java
+++ projects/jasn1-compiler/src/main/java/org/openmuc/jasn1/compiler/Compiler.java
@@ -23,7 +23,7 @@ import org.openmuc.jasn1.compiler.parser.ASNParser;
 
 public class Compiler {
 
-    public final static String VERSION = "1.8.2";
+    public final static String VERSION = "1.8.3-SNAPSHOT";
 
     public static void main(String args[]) throws Exception {
 
