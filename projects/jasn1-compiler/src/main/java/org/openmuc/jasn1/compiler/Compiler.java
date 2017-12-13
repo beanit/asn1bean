5406878ef617c2a4dd95ddeab8b49abf1a4ef57d
Stefan Feuerhahn <stefan@feuerhahn.eu>
2017-12-13 20:14:02 +0100

    release v1.9.0

--- projects/jasn1-compiler/src/main/java/org/openmuc/jasn1/compiler/Compiler.java
+++ projects/jasn1-compiler/src/main/java/org/openmuc/jasn1/compiler/Compiler.java
@@ -23,7 +23,7 @@ import org.openmuc.jasn1.compiler.parser.ASNParser;
 
 public class Compiler {
 
-    public final static String VERSION = "1.8.3-SNAPSHOT";
+    public final static String VERSION = "1.9.0";
 
     public static void main(String args[]) throws Exception {
 
