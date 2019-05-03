34974b33aebad118967ea1b733ca4562a4d87137
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2019-05-03 08:59:02 +0200

    set version to 1.11.2-SNAPSHOT

--- projects/jasn1-compiler/src/main/java/com/beanit/jasn1/compiler/Compiler.java
+++ projects/jasn1-compiler/src/main/java/com/beanit/jasn1/compiler/Compiler.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 public class Compiler {
 
-  public static final String VERSION = "1.11.1";
+  public static final String VERSION = "1.11.2-SNAPSHOT";
 
   public static void main(String[] args) throws Exception {
 
