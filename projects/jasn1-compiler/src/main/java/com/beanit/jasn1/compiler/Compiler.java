9e16223024d92746c8ee49e03bf70307e8d52880
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2019-06-06 10:23:02 +0200

    release v1.11.2

--- projects/jasn1-compiler/src/main/java/com/beanit/jasn1/compiler/Compiler.java
+++ projects/jasn1-compiler/src/main/java/com/beanit/jasn1/compiler/Compiler.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 public class Compiler {
 
-  public static final String VERSION = "1.11.2-SNAPSHOT";
+  public static final String VERSION = "1.11.2";
 
   public static void main(String[] args) throws Exception {
 
