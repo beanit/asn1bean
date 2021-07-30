aadcef094bcb8ed525f17399d32b9c70f022e3fb
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2021-07-30 10:09:36 +0200

    release v1.13.0

--- projects/asn1bean-compiler/src/main/java/com/beanit/asn1bean/compiler/Compiler.java
+++ projects/asn1bean-compiler/src/main/java/com/beanit/asn1bean/compiler/Compiler.java
@@ -34,7 +34,7 @@ import java.util.List;
 
 public class Compiler {
 
-  public static final String VERSION = "1.12.1-SNAPSHOT";
+  public static final String VERSION = "1.13.0";
 
   public static void main(String[] args) throws Exception {
 
