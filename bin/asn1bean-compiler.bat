72609f075c3118918bfb589f1b232e0a6f250237
Stefan Feuerhahn <stefan.feuerhahn@lbd.de>
2023-06-23 20:07:26 +0200

    fix windows run script to work with Java 17

--- bin/asn1bean-compiler.bat
+++ bin/asn1bean-compiler.bat
@@ -1,7 +1,7 @@
 ::BATCH file to windows
-ECHO compiling
+@echo off
 
 set BATDIR=%~dp0
-set LIBDIR="%BATDIR%..\build\libs-all"
+set LIBDIR="%BATDIR%..\build\libs-all\*"
 
-java -Djava.ext.dirs=%LIBDIR% com.beanit.asn1bean.compiler.Compiler %*
+java -cp %LIBDIR% com.beanit.asn1bean.compiler.Compiler %*
