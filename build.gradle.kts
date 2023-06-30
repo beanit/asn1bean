11954a1a056f5c04dab54eccfc99e306ec6ebe85
Stefan Feuerhahn <stefan.feuerhahn@beanit.com>
2023-06-30 21:09:17 +0200

    set version to 1.14.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val sonatypeStagingProfileId: String? by project
 
 //----------- project specific configuration start --------------------
 
-val cfgVersion = "1.14.0"
+val cfgVersion = "1.14.1-SNAPSHOT"
 val cfgGroup = "com.beanit"
 val cfgCopyToRoot = true
 val cfgSignPom = true
